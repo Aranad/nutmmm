b83b7af357a2694f1be18ed0c27920087ecb9c36
nutmmm <nutmmm@organicdesign.co.nz>
2015-12-04 09:02:37 +1300

    Removed the need to load pages more than once.

--- website/js/main.js
+++ website/js/main.js
@@ -1,4 +1,5 @@
 var linkBase = "./";
+var loadedPages = [];
 
 $(document).ready(function(){
 	//no point reinventing the wheel, so we will use this basic image slider plugin to save time
@@ -41,8 +42,6 @@ function loadPage(){
 	//check what page we are at
 	var theUrl = window.location.hash.substr(1).toString().toLowerCase();
 	
-	console.log("./pages/" + theUrl + ".html");
-	
 	if(theUrl == "portfolio"){//check if we are switching to the portfolio page
 		//and then add the extra acknowledgements
 		$(".footerAdition").html(", Image &quot;A Neon Grid Effect Backdrop With City&quot; courtesy of Victor Habbick at FreeDigitalPhotos.net, Image &quot;Bangkok City&quot; courtesy of samarttiw at FreeDigitalPhotos.net");
@@ -52,39 +51,69 @@ function loadPage(){
 		$(".footerAdition").html("");
 	}
 	
-	$.ajax({
-		url: linkBase + "pages/" + theUrl + ".html",
-		dataType: "html",
+	var key = $.inArray(theUrl, loadedPages);
+	
+	if(key > -1){
+		$(".pages").hide();
+		$("." + loadedPages[key]).show();
+		//set page title
+		$("title").text("TK Digital Arts | " + theUrl.substr(0,1).toUpperCase()+theUrl.substr(1));
+		//highlight the page button to show that this page is that page and disable the other highlight
+		$("a.highlight").removeClass("highlight");
+		$("a[href='#" + theUrl + "']").addClass("highlight");
+	}
+	else{
+		console.log("loading " + linkBase + "pages/" + theUrl + ".html");
 		
-		success: function(data) {
-			//set page content
-			$(".content").html(data);
-			//set page title
-			$("title").text("TK Digital Arts | " + theUrl.substr(0,1).toUpperCase()+theUrl.substr(1));
-			//highlight the page button to show that this page is that page and disable the other highlight
-			$("a.highlight").removeClass("highlight");
-			$("a[href='#" + theUrl + "']").addClass("highlight");
-		},
-		
-		error: function(x,e) {
-			//we got an error most probably the page doesn't exist so load home page and log warning
-			$.ajax({
-				url: linkBase + "pages/home.html",
-				dataType: "html", 
+		$.ajax({
+			url: linkBase + "pages/" + theUrl + ".html",
+			dataType: "html",
+			
+			success: function(data) {
+				$(".pages").hide();
+				//record this page as loaded
+				loadedPages.push(theUrl);
+				//set page content
+				$(".content").append(data);
+				//set page title
+				$("title").text("TK Digital Arts | " + theUrl.substr(0,1).toUpperCase()+theUrl.substr(1));
+				//highlight the page button to show that this page is that page and disable the other highlight
+				$("a.highlight").removeClass("highlight");
+				$("a[href='#" + theUrl + "']").addClass("highlight");
+			},
+			
+			error: function(x,e) {
+				var key = $.inArray(theUrl, loadedPages);
 				
-				success: function(data) {
-					$(".content").html(data);
+				if(key > -1){
+					$(".pages").hide();
+					$(".home").show();
 				}
-			});
-			
-			console.warn("Page not found: 'pages/" + theUrl + ".html'");
-			
-			//set homepage as the selected page
-			$("a.highlight").removeClass("highlight");
-			$("a[href='#home']").addClass("highlight");
-			
-			//set page title
-			$("title").text("TK Digital Arts | Home");
-		}
-	});
+				else{
+					//we got an error most probably the page doesn't exist so load home page and log warning
+					$.ajax({
+						url: linkBase + "pages/home.html",
+						dataType: "html", 
+						
+						success: function(data) {
+							$(".pages").hide();
+							//reccord homepage
+							loadedPages.push("home");
+							//set content
+							$(".content").append(data);
+						}
+					});
+				}
+				
+				//set homepage as the selected page
+				$("a.highlight").removeClass("highlight");
+				$("a[href='#home']").addClass("highlight");
+				
+				//set page title
+				$("title").text("TK Digital Arts | Home");
+				
+				console.warn("Page not found: 'pages/" + theUrl + ".html'");
+			}
+		});
+	}
 }
