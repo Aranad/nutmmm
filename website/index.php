d4640bd7953653f947077d56933ee56cf1c3f9c6
nutmmm <nutmmm@organicdesign.co.nz>
2017-01-26 18:02:49 +1300

    Php json pull test

--- website/index.php
+++ website/index.php
@@ -3,7 +3,11 @@
 <body>
 
 <?php
-echo "My first PHP script!";
+//$url = urlencode("http://xyz.com/api?apikey=foo&v1=bar&v2=baz");
+$url = "https://monerodice.net/api/betStats?public_key=CfVBTe3aAEV2YDThBXpzmB3mPKnM3lCu&private_key=Lfj1Hi3Vqr297R98o7xlOC3Qac7CT8Yz&input_limit=1";
+$response = file_get_contents($url);
+
+echo $response;
 ?>
 
 </body>
