2e81b224d12c24315312e233dc51110db34f03a1
nutmmm <nutmmm@organicdesign.co.nz>
2017-01-26 18:08:03 +1300

    Php json pull test - 2

--- website/index.php
+++ website/index.php
@@ -4,10 +4,31 @@
 
 <?php
 //$url = urlencode("http://xyz.com/api?apikey=foo&v1=bar&v2=baz");
-$url = "https://monerodice.net/api/betStats?public_key=CfVBTe3aAEV2YDThBXpzmB3mPKnM3lCu&private_key=Lfj1Hi3Vqr297R98o7xlOC3Qac7CT8Yz&input_limit=1";
+/*$url = "https://monerodice.net/api/betStats?public_key=CfVBTe3aAEV2YDThBXpzmB3mPKnM3lCu&private_key=Lfj1Hi3Vqr297R98o7xlOC3Qac7CT8Yz&input_limit=1";
 $response = file_get_contents($url);
 
-echo $response;
+echo $response;*/
+
+$url = 'http://monerodice.net/api/betStats';
+=&=&=1
+$data = array('public_key' => 'CfVBTe3aAEV2YDThBXpzmB3mPKnM3lCu', 'private_key' => 'Lfj1Hi3Vqr297R98o7xlOC3Qac7CT8Yz', 'input_limit' => '1');
+
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+if ($result === FALSE) { /* Handle error */ }
+else{
+	echo $result;
+}
+
+var_dump($result);
 ?>
 
 </body>
